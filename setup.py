a1bd3b3998735b006b0faac63b9bb10c2f1fe5da
Fabian Spoendlin <f.spoendlin@bluewin.ch>
2024-08-15 15:45:24 +0100

    update readme

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='AbFlex',
+    name='ITsFlexible',
     version='1.0.0',
     author='Fabian C. Spoendlin',
     author_email='fabian.spoendlin@stats.ox.ac.uk',
     description='Prediction of antibody CDR and protein loop flexibility',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/fspoendlin/AbFlex',
-    packages=find_packages('AbFlex', 'AbFlex.*'),
+    url='https://github.com/fspoendlin/ITsFlexible',
+    packages=find_packages('ITsFlexible', 'ITsFlexible.*'),
     install_requires=[
         'biopandas==0.4.1',
         'biopython==1.83',
